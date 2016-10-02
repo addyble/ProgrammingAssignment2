c24d1a50443929f786a1a5ba2d58745747ff600e
addyble <adil.jackson98@gmail.com>
2016-10-02 12:44:47 +0530

    Done with Matrix Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+###makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+ 
+## Elements are put in row-wise order as independent vectors
 
-## Write a short comment describing this function
+Code used:
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x1=numeric,...){
+X<- rbind(x1,...)
+return(X)
 }
 
+###Testing:
+>makeCacheMatrix(c(1,2),c(2,3))
 
-## Write a short comment describing this function
+     [,1] [,2]
+x1    1    2
+        2    3
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+###cacheSolve: This function computes the inverse of the special "matrix"
+
+Code used: cacheSolve<- function(t){
+solve(t)
 }
+
+###Testing:
+>t<-makeCacheMatrix(c(1,2),c(2,3))
+>cacheSolve(t)
+       x1   
+[1,] -3  2
+[2,]  2 -1
